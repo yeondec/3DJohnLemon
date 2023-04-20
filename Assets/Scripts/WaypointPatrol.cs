434eed1a49b28404b3a9e39115aed5f55e2dda77
TaeHyung <rkrp15@gmail.com>
2023-04-20 17:37:32 +0900

    시작점

---
+++ Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointPatrol : MonoBehaviour
+{
+    public NavMeshAgent navMeshAgent;
+    public Transform[] waypoints;
+
+    int m_CurrentWaypoointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent.SetDestination(waypoints[0].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypoointIndex = (m_CurrentWaypoointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypoointIndex].position);
+        }
+    }
+}
